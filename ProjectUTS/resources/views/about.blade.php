f73ba538028f181d1650d47c6a015d56ceea5407
DhimasLuthfi <dhimas3341@gmail.com>
2025-09-20 20:10:42 +0700

    commit 1 instal laravel

---
+++ ProjectUTS/resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman About</title>
+</head>
+<body>
+    <h1>Ini Halaman About</h1>
+    <p>Selamat datang pengunjung sekalian</p>
+
+    <br>
+    <a href="/">Kembali ke Home</a>
+    
+</body>
+</html>
